50b0cd3db79873e5aef882196155e4503ae5a830
Hanzla Nouman <150622496+Hanzla-Nouman@users.noreply.github.com>
2024-06-18 18:04:25 +0500

    Styling Filter Modal Content

--- components/FilterView.jsx
+++ components/FilterView.jsx
@@ -10,21 +10,32 @@ export const SectionView = ({ title, content }) => {
     </View>
   );
 };
-export const CommonFilterView = ({data,filterName,filter,setFilter})=>{
-    return(
-      <View>
-        {
-            data && data.map((item,index)=>{
-                return(
-                    <Pressable>
-                        <Text>{captilize(item)}</Text>
-                    </Pressable>
-                )
-            })
-        }
-      </View>
-    )
+export const CommonFilterView = ({ data, filterName, filter, setfilter }) => {
+  const onSelect = (item)=>{
+    setfilter({...filter,[filterName]:item})
   }
+  return (
+    <View style={styles.flexRowWrap}>
+      {data &&
+        data.map((item, index) => {
+          let isActive = filter && filter[filterName] == item;
+          let backgroundColor = isActive ? theme.colors.neutral(0.7) : "white";
+          let color = isActive ? theme.colors.white : theme.colors.neutral(0.4);
+          return (
+            <Pressable
+            onPress={()=>onSelect(item)}
+              key={index}
+              style={[styles.outlinedButton, {backgroundColor}]}
+            >
+              <Text style={[styles.outlinedButtonText, {color}]}>
+                {captilize(item)}
+              </Text>
+            </Pressable>
+          );
+        })}
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   contentContainer: {
     gap: 8,
@@ -34,4 +45,19 @@ const styles = StyleSheet.create({
     color: theme.colors.neutral(0.8),
     fontWeight: "bold",
   },
+  flexRowWrap: {
+    flexDirection: "row",
+    gap: 10,
+    flexWrap: "wrap",
+  },
+  outlinedButton: {
+    padding: 8,
+    paddingHorizontal: 14,
+    borderWidth: 1,
+    borderColor: theme.colors.grayBg,
+    borderRadius: theme.radius.xs,
+    borderCurve: "continuous",
+  },outlinedButtonText:{
+
+  }
 });
