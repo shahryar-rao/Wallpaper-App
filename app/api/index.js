cd69241badccc30840e91cc485fc9949e394d13c
shahryar-rao <raoshahryar907@gmail.com>
2024-12-03 03:37:46 +0500

    complete

--- app/api/index.js
+++ app/api/index.js
@@ -3,7 +3,7 @@ const API_KEY="44410980-4210ae78ab8f64418d423364d"
 const API_URL=`https://pixabay.com/api/?key=${API_KEY}`
 
 const formatUrl = (params)=>{
-    let url = API_URL+"&per_page=25&safesearch=true&editors_choice=true"
+    let url = API_URL+"&per_page=35&safesearch=true&editors_choice=true"
     if(!params) return url
     let paramKeys = Object.keys(params)
     paramKeys.map(key=>{
